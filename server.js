b129573fe2d01821d21097cf76b3221ad06804a8
RonnieJSanchez <100317073+RonnieJSanchez@users.noreply.github.com>
2022-07-03 10:28:26 -0400

    sending index and show data

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ const app = express()
 app.set('views', __dirname + '/views')
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
+app.use(express.static('public'))
 
 // ROUTES
 app.get('/', (req, res) => {
@@ -20,7 +21,14 @@ app.get('/', (req, res) => {
 const breadsConstroller = require ('./controllers/breads_controller.js')
 app.use('/breads', breadsConstroller)
 
+
 // LISTEN
 app.listen(PORT, () => {
   console.log('nomming at port', PORT);
+})
+
+
+// 404 Page
+app.get('*', (req, res) => {
+  res.send('404')
 })
