05528785f573eca5f0de503c258f644a6671044c
RonnieJSanchez <100317073+RonnieJSanchez@users.noreply.github.com>
2022-07-21 21:52:55 -0400

    Fixing casing

--- views/index.jsx
+++ views/index.jsx
@@ -1,44 +1,38 @@
-const React = require('react')
-const baker_seed = require('../models/baker_seed')
-const Default = require('./layouts/Default')
+const React = require("react");
+const baker_seed = require("../models/baker_seed");
+const Default = require("./layouts/Default");
 
-function Index ({breads, bakers, title}) {
+function Index({ breads, bakers, title }) {
   return (
     <Default title={title}>
-
-  <h2>Index Page</h2>
-  <h3>Bakers</h3>
-  <ul>
-    {bakers.map((baker)=>{
-        return (
-          <li key={baker._id}>
-            <a href={`/bakers/${baker._id}`}>{baker.name}</a>
-          </li>
-        )
-      })
-    }
-  </ul>
-  <h3>Breads</h3>
-  <ul>
-    {
-      breads.map((bread, index)=> {
-        return (
-        <li key={bread._id}>
-            <a href={`/breads/${bread._id}`}>
-                {bread.name}
-            </a>
-        </li>
-        )
-      }) 
-    }
-  </ul>
+      <h2>Index Page</h2>
+      <h3>Bakers</h3>
+      <ul>
+        {bakers.map((baker) => {
+          return (
+            <li key={baker._id}>
+              <a href={`/bakers/${baker._id}`}>{baker.name}</a>
+            </li>
+          );
+        })}
+      </ul>
+      <h3>Breads</h3>
+      <ul>
+        {breads.map((bread, index) => {
+          return (
+            <li key={bread._id}>
+              <a href={`/breads/${bread._id}`}>{bread.name}</a>
+            </li>
+          );
+        })}
+      </ul>
       <div className="newButton">
-        <a href="/breads/new"><button>Add a new bread</button></a>
+        <a href="/breads/new">
+          <button>Add a new bread</button>
+        </a>
       </div>
-</Default>
-
-    )
+    </Default>
+  );
 }
 
-module.exports = Index
-
+module.exports = Index;
